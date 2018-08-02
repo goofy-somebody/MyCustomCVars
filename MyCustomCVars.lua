4fc6e96da63fd5f110048d04e86745d530976195
forumuser599 <forumuser599@outlook.com>
2018-08-02 14:53:19 -0500

    first commit

---
+++ MyCustomCVars.lua
@@ -0,0 +1,36 @@
+-- local cameraDistanceMaxZoomFactor=GetCVar("cameraDistanceMaxZoomFactor")
+-- local autoQuestWatch=GetCVar("autoQuestWatch")
+-- local advancedWatchFrame=GetCVar("advancedWatchFrame")
+-- local synchronizeSettings=GetCVar("synchronizeSettings")
+-- local SpellQueueWindow=GetCVar("SpellQueueWindow")
+
+-- local cameraDistanceMaxZoomFactorValue="2.6"
+-- local autoQuestWatchValue="1"
+-- local advancedWatchFrameValue="1"
+-- local synchronizeSettingsValue="0"
+-- local SpellQueueWindowValue="50"
+
+-- if cameraDistanceMaxZoomFactor~=cameraDistanceMaxZoomFactorValue then
+	-- SetCVar("cameraDistanceMaxZoomFactor",cameraDistanceMaxZoomFactorValue)
+	--message("Adjusted the camera zoom factor.")
+-- end
+
+-- if autoQuestWatch~=autoQuestWatchValue then
+	-- SetCVar("autoQuestWatch",autoQuestWatchValue)
+	--message("Auto Quest Watch turned on.")
+-- end
+
+-- if advancedWatchFrame~=advancedWatchFrameValue then
+	-- SetCVar("advancedWatchFrame",advancedWatchFrameValue)
+	-- message("Advanced Watch Frame turned on.")
+-- end
+
+-- if synchronizeSettings~=synchronizeSettingsValue then
+	-- SetCVar("synchronizeSettings",synchronizeSettingsValue)
+	--message("No longer synchronizing settings.")
+-- end
+
+-- if SpellQueueWindow~=SpellQueueWindowValue then
+	-- SetCVar("SpellQueueWindow",SpellQueueWindowValue)
+	-- message("Lag tolerance adjusted.")
+-- end
